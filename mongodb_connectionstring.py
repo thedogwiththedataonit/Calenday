6dfe020508ab6cc97f745ec7b3b012e565cf97c9
Junseo Park <parkaph@bc.edu>
2022-01-02 16:14:11 -0800

    changed db

--- mongodb_connectionstring.py
+++ mongodb_connectionstring.py
@@ -1,5 +1,5 @@
-connection_string = "mongodb+srv://blockchaingame:0806@blockchaingame.mqffw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+connection_string = "mongodb+srv://blockchaingame:0806@cluster0.mqffw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 #mongoengine.connect(
     #host='mongodb+srv://<username>:<pass>@<host_name>/<database>?authSource=admin&connectTimeoutMS=60000'
